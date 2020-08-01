f6cd19567988c31e0e6911f24f2b9b67ac55c220
clicktoinstall <69070384+clicktoinstall@users.noreply.github.com>
2020-08-01 16:27:35 +0530

    Add files via upload

---
+++ basic_tool_for_hacking.py
@@ -0,0 +1,139 @@
+#!usr/bin/evn python3
+
+import subprocess
+
+linux = input("what's your os  linux give sudo , termux give enter ==>")
+
+subprocess.call(linux + " apt install figlet", shell=True)
+
+subprocess.call("figlet  lets  install   basic   tools  for  hacking ", shell=True)
+
+subprocess.call(linux + " apt install python", shell=True)
+
+subprocess.call("y", shell=True)
+
+subprocess.call(linux + " apt install git", shell=True)
+
+subprocess.call("y", shell=True)
+
+subprocess.call(linux + " apt install wget", shell=True)
+
+subprocess.call("y", shell=True)
+
+subprocess.call(linux + " apt install python3-pip", shell=True)
+
+subprocess.call("y", shell=True)
+
+subprocess.call(linux + " apt install python3", shell=True)
+
+subprocess.call("y", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/sherlock-project/sherlock", shell=True)
+
+subprocess.call(linux + " cd sherlock", shell=True)
+
+subprocess.call(linux + " python3 -m pip install -r requirements.txt", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/TamilHackz/RootMe", shell=True)
+
+subprocess.call(linux + " cd RootMe", shell=True)
+
+subprocess.call(linux + " chmod 777 rootme.sh", shell=True)
+
+subprocess.call(linux + " bash rootme.sh ", shell=True)
+
+subprocess.call(linux + " 1", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/thewhiteh4t/seeker", shell=True)
+
+subprocess.call(linux + " cd seeker", shell=True)
+
+subprocess.call(linux + " chmod 777 termux_install.sh", shell=True)
+
+subprocess.call(linux + " ./termux_install.sh", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/maldevel/IPGeoLocation", shell=True)
+
+subprocess.call(linux + " pip3 install colorama", shell=True)
+
+subprocess.call(linux + " pip3 install termcolor", shell=True)
+
+subprocess.call(linux + " pip3 install -r requirements.txt --user", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/metachar/PhoneSploit", shell=True)
+
+subprocess.call(linux + " cd PhoneSploit", shell=True)
+
+subprocess.call(linux + " pip install colorama", shell=True)
+
+subprocess.call(linux + " python2 main_linux.py", shell=True)
+
+subprocess.call(linux + " sudo apt install android-tools-adb android-tools-fastboot", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/evait-security/weeman", shell=True)
+
+subprocess.call(linux + " apt install python2", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/ultrasecurity/webkiller.git", shell=True)
+
+subprocess.call(linux + " cd webkiller", shell=True)
+
+subprocess.call(linux + " pip3 install -r requirements.txt", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/rand0m1ze/ezsploit", shell=True)
+
+subprocess.call(linux + "chmod 777 ezsploit.sh", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/TheSpeedX/TBomb", shell=True)
+
+subprocess.call(linux + " cd TBomb", shell=True)
+
+subprocess.call(linux + " chmod +x TBomb.sh", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/issamelferkh/userrecon", shell=True)
+
+subprocess.call(linux + " chmod +x userrecon.sh", shell=True)
+
+subprocess.call(linux + " apt install curl ", shell=True)
+
+subprocess.call(" y", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/Sup3r-Us3r/HashCode", shell=True)
+
+subprocess.call(linux + " cd HashCode", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " git clone https://github.com/KALILINUXTRICKSYT/easysploit.git", shell=True)
+
+subprocess.call(linux + " cd easysploit", shell=True)
+
+subprocess.call(linux + " chmod +x installer.sh", shell=True)
+
+subprocess.call(linux + " bash installer.sh", shell=True)
+
+subprocess.call("cd ..", shell=True)
+
+subprocess.call(linux + " ", shell=True)
+
+subprocess.call(linux + " figlet bye by SRIKANTH.N", shell=True)
